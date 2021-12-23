642c9d6407c06d41451e749b70c40fa24e4dfeee
Kirk Garrison <Kirkgarrison24601@gmail.com>
2021-12-23 15:24:38 -0800

    completed CC19

---
+++ python/tests/test_odd_sum_binary.py
@@ -0,0 +1,10 @@
+from code_challenges.odd_sum_binary.odd_sum_binary import newNode, addBT
+
+def test_is_newNode():
+    assert newNode
+
+
+def test_is_sum():
+    odd = newNode(3)
+    odd = newNode(5)
+    assert odd.key == 8
