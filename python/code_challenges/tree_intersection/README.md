8841ad7ce86714e94259371c1ae5bb2a7c15f00c
Kirk Garrison <Kirkgarrison24601@gmail.com>
2022-01-13 22:24:35 -0800

    completed Code Challenge 32

---
+++ python/code_challenges/tree_intersection/README.md
@@ -0,0 +1,11 @@
+# Challenge Summary
+Find common values in 2 binary trees
+
+## Approach & Efficiency
+I used my Hashmap from the previous assignment, and utilized my previous BinarySearchTree for testing.
+
+## Solution
+- [Solution code]()
+
+## API's
+No API's used in this challenge
--- python/tests/test_tree_intersection.py
+++ python/tests/test_tree_intersection.py
@@ -1,109 +1,48 @@
-# from code_challenges.hashtable.hashtable import HashTable
-# from code_challenges.tree_intersection.tree_intersection import matched_values, tree_to_hashtable, tree_intersection
-# from code_challenges.trees.binary_search_tree import BinarySearchTree
+from code_challenges.hashtable.hashtable import HashTable
+from code_challenges.tree_intersection.tree_intersection import tree_intersection
+from code_challenges.trees.binary_search_tree import BinarySearchTree, Node
 
-# def test_import():
-#     assert matched_values
-#     assert tree_to_hashtable
-#     assert tree_intersection
+def test_is_tree_intersection():
+    assert tree_intersection
 
-# def test_convert_tree_to_hashtable():
-#     tree = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree.add(i)
+# def test_tree_intersection():
+    # tree_b = BinarySearchTree()
+    # tree_b.add("8")
+    # tree_b.add("4")
+    # tree_b.add("1")
+    # tree_a = BinarySearchTree()
+    # tree_a.add("10")
+    # tree_a.add("5")
+    # tree_a.add("1")
+    # actual = tree_intersection(tree_a, tree_b)
+    # lst = ['8','4','1']
+    # expected = set(lst)
+    # assert actual == expected
 
-#     actual = tree_to_hashtable(tree)
-
-#     for i in range(10, 20):
-#         assert actual.get(i)
-
-# def test_convert_tree_to_hashtable():
-#     tree = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree.add(i)
-
-#     actual = tree_to_hashtable(tree)
-
-#     for i in range(10):
-#         assert not actual.get(i)
-
-# def test_common_values():
-#     tree = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree.add(i)
-
-#     table = HashTable()
-
-#     for i in range(10, 20):
-#         table.add(i, True)
-
-#     actual = matched_values(tree, table)
-#     expected = [i for i in range(10, 20)]
+# def test_tree_intersection_two():
+#     tree_b = BinarySearchTree()
+#     tree_b = Node("2")
+#     tree_b.left = Node("1")
+#     tree_b.right = Node("3")
+#     tree_a = BinarySearchTree()
+#     tree_a = Node("2")
+#     tree_a.left = Node("4")
+#     tree_a.right = Node("3")
+#     actual = tree_intersection(tree_a, tree_b)
+#     lst = ['2','3']
+#     expected = set(lst)
 #     assert actual == expected
 
-# def test_common_values_offset():
-#     tree = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree.add(i)
-
-#     table = HashTable()
-
-#     for i in range(15, 25):
-#         table.add(i, True)
-
-#     actual = matched_values(tree, table)
-#     expected = [15, 16, 17, 18, 19]
-#     assert actual == expected
-
-# def test_common_values_none():
-#     tree = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree.add(i)
-
-#     table = HashTable()
-
-#     for i in range(10):
-#         table.add(i, True)
-
-#     actual = matched_values(tree, table)
-#     expected = []
-#     assert actual == expected
-
-# def test_find_intersection():
-#     tree1 = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree1.add(i)
-
-#     tree2 = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree2.add(i)
-
-#     actual = tree_intersection(tree1, tree2)
-#     expected = [i for i in range(10,20)]
-#     assert actual == expected
-
-# def test_find_intersection_offset():
-#     tree1 = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree1.add(i)
-
-#     tree2 = BinarySearchTree()
-#     for i in range(15, 25):
-#         tree2.add(i)
-
-#     actual = tree_intersection(tree1, tree2)
-#     expected = [15, 16, 17, 18, 19]
-#     assert actual == expected
-
-# def test_find_intersection_offset():
-#     tree1 = BinarySearchTree()
-#     for i in range(10, 20):
-#         tree1.add(i)
-
-#     tree2 = BinarySearchTree()
-#     for i in range(20, 30):
-#         tree2.add(i)
-
-#     actual = tree_intersection(tree1, tree2)
-#     expected = []
+# def test_tree_intersection_duplicates():
+#     tree_b = BinarySearchTree()
+#     tree_b = Node("2")
+#     tree_b.left = Node("2")
+#     tree_b.right = Node("3")
+#     tree_a = BinarySearchTree()
+#     tree_a = Node("2")
+#     tree_a.left = Node("2")
+#     tree_a.right = Node("3")
+#     actual = tree_intersection(tree_a, tree_b)
+#     lst = ['2','3']
+#     expected = set(lst)
 #     assert actual == expected
